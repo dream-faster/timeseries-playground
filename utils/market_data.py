98f3fec04e336506bd125be0ce549b17001c02be
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-12-20 16:27:06 +0100

    fix(Data): use yfinance to download data

--- utils/market_data.py
+++ utils/market_data.py
@@ -4,34 +4,72 @@ import pandas_datareader.data as web
 import pandas as pd
 import datetime
 import seaborn as sns
-sns.set(rc={'figure.figsize':(11.7,8.27)})
+import yfinance as yf
+
+yf.pdr_override()
+sns.set(rc={"figure.figsize": (11.7, 8.27)})
+
 
 def get_market_data() -> pd.DataFrame:
-
-    def clean_yfinance_data(df) -> pd.Series:
-        df = df['Adj Close']
-        df = df.rename('Close')
-        return df
-
     def take_returns(series: pd.Series) -> pd.Series:
         return series.pct_change()
 
-    start_date = datetime.datetime(1990, 1, 1)
-    end_date = datetime.datetime(2022,10,16)
+    start_date = "1990-01-01"
+    end_date = "2022-10-16"
 
-    sp500 = web.DataReader('^GSPC', 'yahoo', start_date, end_date)['Adj Close'].rename('SP500')
-    sp500_returns = take_returns(web.DataReader('^GSPC', 'yahoo', start_date, end_date)['Adj Close']).rename('SP500_returns')
-    sp500_std = sp500_returns.rolling(21).std().rename('SP500_std')
-    gld_returns = take_returns(web.DataReader('GOLD', 'yahoo', start_date, end_date)['Adj Close']).rename('GOLD_returns')
-    vix = web.DataReader('VIXCLS', 'fred', start_date, end_date)['VIXCLS'].rename('VIX')
+    sp500 = (
+        yf.Ticker("^GSPC")
+        .history(interval="1d", start=start_date, end=end_date)["Close"]
+        .rename("SP500")
+    )
+    sp500_returns = take_returns(sp500).rename("SP500_returns")
+    sp500_std = sp500_returns.rolling(21).std().rename("SP500_std")
+    gld_returns = take_returns(
+        yf.Ticker("GOLD").history(interval="1d", start=start_date, end=end_date)[
+            "Close"
+        ]
+    ).rename("GOLD_returns")
+
+    vix = (
+        yf.Ticker("^VIX")
+        .history(interval="1d", start=start_date, end=end_date)["Close"]
+        .rename("VIX")
+    )
     vix_returns = take_returns(vix).rename("VIX_returns")
-    y30 = web.DataReader('DGS30', 'fred', start_date, end_date)['DGS30'].rename('Y30')
+    y30 = (
+        yf.Ticker("^TYX")
+        .history(interval="1d", start=start_date, end=end_date)["Close"]
+        .rename("Y30")
+    )
     y30_returns = take_returns(y30).rename("Y30_returns")
-    ndx = web.DataReader("^NDX", 'yahoo', start_date, end_date)['Adj Close'].rename('NDX')
+    ndx = (
+        yf.Ticker("^NDX")
+        .history(interval="1d", start=start_date, end=end_date)["Close"]
+        .rename("NDX")
+    )
     ndx_returns = take_returns(ndx).rename("NDX_returns")
 
-    df = pd.concat([sp500, sp500_returns, sp500_std, gld_returns, vix, vix_returns, y30, y30_returns, ndx, ndx_returns], axis=1).fillna(method='ffill').dropna()
+    df = (
+        pd.concat(
+            [
+                sp500,
+                sp500_returns,
+                sp500_std,
+                gld_returns,
+                vix,
+                vix_returns,
+                y30,
+                y30_returns,
+                ndx,
+                ndx_returns,
+            ],
+            axis=1,
+        )
+        .fillna(method="ffill")
+        .dropna()
+    )
     print(f" {df.shape[0]} {df.shape[1]}")
     return df
+
 
 # %%
