91a095a3d41eb10dec986cff2ebc800572456ca1
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-01-02 11:52:45 +0100

    fix(River): trying to get the pipeline to work (but failing)

--- train_river.py
+++ train_river.py
@@ -12,18 +12,31 @@ from tqdm import tqdm
 from river import stream
 
 X_y = stream.iter_csv(
-    "data/energy.csv", target="P", parse_dates={"time": "%Y-%m-%d %H:%M:%S"}
+    "data/energy.csv",
+    target="P",
+    parse_dates={
+        "time": "%Y-%m-%d %H:%M:%S",
+    },
+    converters={
+        "load": float,
+        "Gb(i)": float,
+        "Gd(i)": float,
+        "H_sun": float,
+        "T2m": float,
+        "WS10m": float,
+        "P": float,
+        "residual_load": float,
+    },
 )
 
 
 # model = compose.Pipeline(("arimax", time_series.SNARIMAX(1, 2, 3, 4, 5, 6, 7)))
-model = compose.Pipeline(("arimax", time_series.HoltWinters(alpha=0.3,beta=0.1, gamma=0.6,seasonality=12,multiplicative=True)))
-
-print(
-    evaluate.progressive_val_score(
-        model=model,
-        dataset=datasets.AirlinePassengers(),
-        metric=metrics.RMSE(),
-        print_every=10,
-    )
+model = time_series.HoltWinters(
+    alpha=0.3, beta=0.1, gamma=0.6, seasonality=12, multiplicative=True
 )
+model = time_series.SNARIMAX(1, 1, 1)
+
+for x, y in tqdm(X_y):
+    print(x, y)
+
+print(time_series.evaluate(X_y, model, metrics.RMSE(), 1))
